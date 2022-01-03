54380a4d2bc2a9630216a553f564fc145961ab94
Bedirhan <53793285+bedirhanbuyukoz@users.noreply.github.com>
2022-01-03 16:42:14 +0300

    Update main.py
    bugs fixed

--- main.py
+++ main.py
@@ -7,39 +7,44 @@ import tensorflow as tf
 import tensorflow_hub as hub
 
 
-TF_MODEL_URL = 'https://tfhub.dev/google/on_device_vision/classifier/landmarks_classfier_asia_V1/1'
+TF_MODEL_URL = 'https://tfhub.dev/google/on_device_vision/classifier/landmarks_classifier_asia_V1/1'
 LABEL_MAP_URL = 'https://www.gstatic.com/aihub/tfhub/labelmaps/landmarks_classifier_asia_V1_label_map.csv'
-IMAGE_SHAPE = (321,321)
+IMAGE_SHAPE = (321, 321)
 
 
-classifier = tf.keras.Squential([hub.KerasLayer(TF_MODEL_URL,input_shape=IMAGE_SHAPE+(3),output_key="prediction:logits")])
+classifier = tf.keras.Sequential([hub.KerasLayer(TF_MODEL_URL,
+                                                 input_shape=IMAGE_SHAPE+(3,),
+                                                 output_key="predictions:logits")])
 
 
-df = pd.read_cvs(LABEL_MAP_URL)
-label_map = dict(zip(df.id,df.name))
-img_loc ="Image.jpeg"
+df = pd.read_csv(LABEL_MAP_URL)
+label_map = dict(zip(df.id, df.name))
+img_loc = "Image.jpeg"
 img = Image.open(img_loc).resize(IMAGE_SHAPE)
+img
 img = np.array(img)/255.0
 img.shape
 
+img = img[np.newaxis, ...]
+img.shape
 result = classifier.predict(img)
 result
 label_map[np.argmax(result)]
 
+
 class_names=list(label_map.values())
+
 def classify_image(image):
-  img = np.array(image)/255.0
-  img = img[np.newaxis,...]
-  prediction = classifier.predict(img)
-  return label_map[np.argmax(prediction)]
+    img = np.array(image)/255.0
+    img = img[np.newaxis, ...]
+    prediction = classifier.predict(img)
+    return label_map[np.argmax(prediction)]
 
-
-image = gr.inputs.Image(shape=(321,321))
+image = gr.inputs.Image(shape=(321, 321))
 label = gr.outputs.Label(num_top_classes=1)
 
-
 gr.Interface(
-  classify_image,
-  image,
-  label,
-  capture_session=True).launch(debug=True);
+    classify_image,
+    image,
+    label,
+    capture_session=True).launch(debug=True);
